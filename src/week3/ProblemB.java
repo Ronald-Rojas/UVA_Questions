c51e6ab7491beacfc4cccc629a2928852c25ea31
RonZenUbu <ronladred@gmail.com>
2016-01-06 16:54:47 -0500

    completed problem B week 3

--- src/week3/ProblemB.java
+++ src/week3/ProblemB.java
@@ -14,7 +14,31 @@ public class ProblemB {
 			for(String num: cardArray){
 				cardStr+=num;
 			}
+			String nonDoubleStr = "";
+			String doubleStr = "";
+			int sum = 0;
+			for( int digitIndex = 1; digitIndex <cardStr.length(); digitIndex+=2){
+				nonDoubleStr+=cardStr.charAt(digitIndex);
+			}
+			for (int digitIndex = 0; digitIndex <cardStr.length(); digitIndex +=2){
+				doubleStr+=cardStr.charAt(digitIndex);
+			}
 			
+			for(int digitIndex = 0; digitIndex< doubleStr.length(); digitIndex++){
+				int digit = Integer.parseInt(""+ doubleStr.charAt(digitIndex));
+				digit*=2;
+				String digitStr = "" + digit;
+			
+				for(int i = 0; i < digitStr.length(); i++){
+					sum+= Integer.parseInt("" + digitStr.charAt(i));
+				}
+			}
+			for(int digitIndex = 0; digitIndex<nonDoubleStr.length(); digitIndex++){
+				sum+= Integer.parseInt("" + nonDoubleStr.charAt(digitIndex));
+			}
+			String sumStr = "" +sum;
+			if(sumStr.charAt(sumStr.length() -1) =='0') System.out.println("Valid");
+			else System.out.println("Invalid");
 			
 		}
 	}
