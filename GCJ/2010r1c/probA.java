7815b00d99fb495c6fabdd85b270fd0f9f9b66dc
Ronald Rojas <ronladred@gmail.com>
2016-04-11 13:44:43 -0400

    renamed files and created GCJ template

--- GCJ/2010r1c/probA.java
+++ GCJ/2010r1c/probA.java
@@ -1,4 +1,3 @@
-package codejam;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
---
+++ GCJ/template.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class template{
+    public static void main(String[] args) throws NumberFormatException, IOException{
+        BufferedReader in = new BufferedReader( new FileReader("input.txt"));
+        int numinput = Integer.parseInt(in.readLine());
+        File file = new File("output.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for( int input = 0; input < numinput; input++){
+
+
+        }
+        bw.close();
+    }
+}
